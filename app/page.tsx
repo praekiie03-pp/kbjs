1551188da28f63e66bf8e422dd5311affb2f2f7b
NiceKiie <niceKiie@example.com>
2025-11-23 03:21:25 +0700

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans" style={{ backgroundColor: '#D9A8FF' }}>
       <h1 className="text-15xl font-bold text-black">
-        Mr_Ryder
+      Hello Mr_Ryder
       </h1>
     </div>
   );
